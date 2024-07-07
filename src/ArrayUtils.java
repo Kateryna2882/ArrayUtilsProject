22e24cd79046be39a1677e72332336f8acd76629
Kateryna2882 <govoryhina82@gmail.com>
2024-07-07 17:44:48 +0300

    Add methods

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,3 +1,27 @@
 public class ArrayUtils {
+public static double calculateAverage(int[] array) {
+    if (array.length == 0) {
+        throw new IllegalArgumentException("Array cannot be empty");
+    }
+
+    double sum = 0;
+    for (int num : array) {
+        sum += num;
+    }
+    return sum / array.length;
+}
+    public static boolean isSquareMatrix(int[][] matrix) {
+        if (matrix == null) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        for (int[] row : matrix) {
+            if (row.length != rows) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
